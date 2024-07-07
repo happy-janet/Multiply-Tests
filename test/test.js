5e2d0f64af831d8f30069c6864170272d1bfed56
Aballo Janet <jabalo@student.refactory.academy>
2024-07-07 13:54:17 +0300

    add third test

--- test/test.js
+++ test/test.js
@@ -8,5 +8,8 @@ describe ("multiply", () =>{
     it("multiply 2*2 to 4", () =>{
         assert.equal(multiply(2,2), 4)
     })
+    it("multiply 3*3 to 9", () =>{
+        assert.equal(multiply(3,3), 9)
+    })
 });
 
