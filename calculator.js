8c99b9bb6573eedd184a8914ed61c965aecc3197
Aballo Janet <jabalo@student.refactory.academy>
2024-07-09 18:58:35 +0300

    sixth test

--- calculator.js
+++ calculator.js
@@ -2,7 +2,10 @@ export function getTaxValue(a){
     if (a < 12000) {
         return 0;
     }
-    else {
+    else if (a <= 36000){
         return 0.2 * a;
+    }
+    else {
+        return 0.4 * a;
     }
 }
