123fa0b773594f83e4747fbf4173de572305a5cb
Aballo Janet <jabalo@student.refactory.academy>
2024-07-09 18:45:36 +0300

    third test

--- test/calculatortest.js
+++ test/calculatortest.js
@@ -5,4 +5,7 @@ describe ("calculator", () =>{
     it("tax Value of 10000 is 0", () =>{
         assert.equal(getTaxValue(10000), 0)
     });
+    it("tax Value of 12000 is 2400", () =>{
+        assert.equal(getTaxValue(12000), 2400)
+    });
 })
